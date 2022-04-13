3ee49fccd9855f7c17cce64945367f60b1f253b0
grady.white <grady.white@maine.edu>
2022-04-12 21:01:58 -0400

    Tested headers

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from interface import Address_book, Receiver
+from interface import Address_book, Receiver, Header
 import gc
 
 def main():
@@ -11,6 +11,10 @@ def main():
     R.Address.write_book()
     A = Address_book('addressbook.txt')
     print(A)
+
+    H = Header(1234,0,3,32456,32457,64,2)
+
+    print(H)
 
 if __name__ == "__main__":
     main()
